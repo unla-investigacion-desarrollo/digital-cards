a0d48016cb6acb115f17b1da87283b3cb4640b8a
Pablo Vieyra <Pablin_vieyra1@outlook.com>
2023-09-03 14:50:55 -0300

    Add: @Media in Digital card

--- frontend/src/components/DigitalCard.tsx
+++ frontend/src/components/DigitalCard.tsx
@@ -28,29 +28,37 @@ const DigitalCard = ({
 }: Props) => {
   return (
     <div className="flex flex-1 items-center justify-center  h-[80vh] ">
-      <Card className=" w-[550px]  " id="digitalCard">
-        <CardHeader className="p-6 flex-row items-start  ">
-          <div className="w-[200px] max-w-[200px]">
-            <h4 className="font-bold text-3xl">{name}</h4>
-            <p className="text-lg font-bold  ">{position}</p>
+      <Card
+        className="max-sm:w-[340px] max-sm:h-[340px]   w-[550px]"
+        id="digitalCard"
+      >
+        <CardHeader className="max-sm:p-4 p-6 flex-row items-start  ">
+          <div className="max-sm:w-[150px] w-[200px] max-w-[200px]">
+            <h4 className="max-sm:text-xl font-bold text-3xl">{name}</h4>
+            <p className="max-sm:text-base text-lg font-bold  ">{position}</p>
           </div>
-          <div className="ml-[188px]">
+          <div className="max-sm:ml-[60px] max-sm:w-[100px]  ml-[188px]">
             <Image
               alt="Card background"
-              className="object-cover rounded-xl grid justify-items-end"
+              className="object-cover rounded-xl grid justify-items-end "
               src={imageProfile}
               width={120}
             />
           </div>
         </CardHeader>
 
-        <CardBody className="overflow-visible py-2 grid grid-cols-2 mb-4">
+        <CardBody className=" overflow-visible py-2 grid grid-cols-2 mb-4">
           <div>
-            <small className="text-sm font-semibold mt-3">Materias</small>
+            <small className="max-sm:text-xs text-sm font-semibold mt-3">
+              Materias
+            </small>
             <ul className="flex flex-col">
               {materias.map((materia: string) => {
                 return (
-                  <small className="text-default-500 mt-1" key={materia}>
+                  <small
+                    className=" max-sm:text-xs  text-default-500 mt-1"
+                    key={materia}
+                  >
                     {materia}
                   </small>
                 );
