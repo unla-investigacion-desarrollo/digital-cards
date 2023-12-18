cb63928cdfccf6cda7ecebb51cd8827385e71d14
pvieyra <pablinvieyra1@gmail.com>
2023-12-18 01:40:42 -0300

    Add config cors in Spring Application

--- backend/src/main/java/com/api/unlatestcareer/UnlaTestCareerApplication.java
+++ backend/src/main/java/com/api/unlatestcareer/UnlaTestCareerApplication.java
@@ -1,12 +1,30 @@
-package com.api.unlatestcareer;
+	package com.api.unlatestcareer;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.web.servlet.config.annotation.CorsRegistry;
+	import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+	import org.springframework.context.annotation.Bean;
 
- @SpringBootApplication
-public class UnlaTestCareerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnlaTestCareerApplication.class, args);
+	@SpringBootApplication
+	public class UnlaTestCareerApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(UnlaTestCareerApplication.class, args);
+		}
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+					 .allowedOrigins("http://localhost:3000")
+					 .allowedMethods("GET", "POST", "PUT", "DELETE")
+					 .maxAge(3600);
+				}
+
+			};
+		}
 	}
-}
