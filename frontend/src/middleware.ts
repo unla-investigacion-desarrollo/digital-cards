8b46e3935e46ffbd94dc14f3154c308cdc3f305d
pabloVieyra <pablin_vieyra1@outlook.com>
2024-02-14 23:54:12 -0300

    changes in protected routes and add new-user page

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,9 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   const role = request.cookies.get("role")?.value;
-  const protectedRoutes = ["/home"];
-  const adminRoutes = ["/change-password"];
-  const publicRoutes = ["/", "digital-card"];
+  const protectedRoutes = [
+    "/home",
+    "/change-password",
+    "/form-card",
+    "/dashboard",
+  ];
+  const adminRoutes = ["/new-user"];
 
   if (protectedRoutes.includes(request.nextUrl.pathname) && !token) {
     const response = NextResponse.redirect(new URL("/login", request.url));
